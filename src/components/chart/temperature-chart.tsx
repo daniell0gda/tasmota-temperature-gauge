8919087d0461d1a82d08ec273dfcf2cffe15491f
Daniel <ferens.daniel@gmail.com>
2020-12-25 22:04:40 +0100

    ApexChart WiP
    Trying to use Apex Chart

--- src/components/chart/temperature-chart.tsx
+++ src/components/chart/temperature-chart.tsx
@@ -1,7 +1,8 @@
 import {Component, Element, h, Host, Method, Prop} from '@stencil/core';
 import {HTMLStencilElement} from '@stencil/core/internal';
-import Chart from 'chart.js';
-import moment from 'moment';
+
+import {ConsoleStorage} from '../../global/consoleStorage';
+import ApexCharts from 'apexcharts';
 
 @Component({
   tag: 'temperature-chart',
@@ -10,67 +11,143 @@ export class TemperatureChart {
 
   @Element() el: HTMLStencilElement;
   @Prop() _temps: string = '';
-  @Prop() _min:number = 10;
-  @Prop() _max:number = 30;
-  private canvasElement: HTMLCanvasElement | undefined;
-  private temperatures: number[] = [];
-  private chart: Chart | undefined;
+  @Prop() _min: number = 10;
+  @Prop() _max: number = 30;
+  chartData: number[][] = [];
+  private storage: ConsoleStorage = new ConsoleStorage();
+  private chartDivElement: HTMLDivElement | undefined;
+  private chartScaleDivElement: HTMLDivElement | undefined;
+  private mainChart:ApexCharts;
+  private scaleChart:ApexCharts;
 
-  componentDidLoad(): void {
-    const ctx = this.canvasElement.getContext('2d');
+  async componentWillLoad(): Promise<void> {
+    const logs = await this.storage.getTemperatures();
+    for (const log of logs) {
+      const point = [log.date.getTime(), log.temp];
+      this.chartData.push(point);
+    }
+  }
 
-    this.chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: 'line',
+  async componentDidLoad(): Promise<void> {
 
-      // The data for our dataset
-      data: {
-        datasets: [{
-          label: 'Temperatura',
-
-          borderColor: 'rgb(255, 99, 132)',
-
-        }]
-      },
-
-      // Configuration options go here
-      options: {
-        scales: {
-          x: {
-            type: 'time',
-            min: new Date('2019-01-01').valueOf(),
-            max: new Date('2021-12-31').valueOf()
-          },
-          y: {
-            type: 'linear',
-            min: 0,
-            max: 100
-          }
-        } as any
-      }
-    });
+   setTimeout(async()=>{
+     await this.setChartOptions(this.chartData);
+     await this.setChartScale(this.chartData);
+   });
   }
 
   componentDidUnload(): void {
-    this.chart.destroy();
+
   }
 
   render(): any {
     return <Host
-      class={{
-        'component-flex-container': true,
-        'height-100': true
-      }}
+
     >
-      <canvas ref={(el: HTMLCanvasElement | undefined) => this.canvasElement = el as HTMLCanvasElement}></canvas>
+      <apex-chart
+        type="bar"
+        series={[{
+          name: 'sales',
+          data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+        }]}
+        options={{
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+          }
+        }}/>
+      <div id="abc" ref={(el: HTMLDivElement | undefined) => this.chartDivElement = el as HTMLDivElement}/>
+      <div id="abcc" ref={(el: HTMLDivElement | undefined) => this.chartScaleDivElement = el as HTMLDivElement}/>
     </Host>;
   }
 
   @Method()
   async update(temp: number, date: Date): Promise<void> {
-    this.chart.data.labels.push(moment(date).format('HH:mm:ss'));
-    this.chart.data.datasets[0].data.push(temp);
+    this.chartData.push([date.getTime(), temp]);
+    this.mainChart.updateSeries([{
+      name: 'Sales',
+      data: this.chartData as any
+    }]);
+    this.scaleChart.updateSeries([{
+      name: 'Sales',
+      data: this.chartData as any
+    }]);
+  }
 
-    this.chart.update();
+  private async setChartOptions(chartData: number[][]): Promise<void> {
+    let options = {
+      series: [{
+        data: chartData
+      }],
+      chart: {
+        id: 'chart2',
+        type: 'line',
+        height: 230,
+        toolbar: {
+          autoSelected: 'pan',
+          show: false
+        }
+      },
+      colors: ['#546E7A'],
+      stroke: {
+        width: 3
+      },
+      dataLabels: {
+        enabled: false
+      },
+      fill: {
+        opacity: 1,
+      },
+      markers: {
+        size: 0
+      },
+      xaxis: {
+        type: 'datetime'
+      }
+    };
+    this.mainChart = new ApexCharts(this.chartDivElement, options);
+    await this.mainChart.render();
+  }
+  private  async setChartScale(chartData: number[][]): Promise<void> {
+    const optionsLine = {
+      series: [{
+        data: chartData
+      }],
+      chart: {
+        id: 'chart1',
+        height: 130,
+        type: 'area',
+        brush: {
+          target: 'chart2',
+          enabled: true
+        },
+        selection: {
+          enabled: true,
+          // xaxis: {
+          //   min: new Date('19 Jun 2017').getTime(),
+          //   max: new Date('14 Aug 2017').getTime()
+          // }
+        },
+      },
+      colors: ['#008FFB'],
+      fill: {
+        type: 'gradient',
+        gradient: {
+          opacityFrom: 0.91,
+          opacityTo: 0.1,
+        }
+      },
+      xaxis: {
+        type: 'datetime',
+        tooltip: {
+          enabled: false
+        }
+      },
+      yaxis: {
+        tickAmount: 2
+      }
+    };
+
+    this.scaleChart = new ApexCharts(this.chartScaleDivElement, optionsLine);
+    await this.scaleChart.render();
   }
 }
